cf1e7ccdedaabd9652af0914afc0a75808fe1d36
William-Newson-Student <wine2a0@student.oaisd.org>
2024-10-14 17:59:19 +0000

    added basic template for tommorow

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("what operator?");
+string? opp = Console.ReadLine();
+if (opp == "+") 
+{
+    Console.WriteLine("Provide 1 number to perform the addition operation");
+    string? Num1 = Console.ReadLine();
+    Console.WriteLine(Num1 + " …got it. Provide a number to add to " + Num1);
+    string? Num2 = Console.ReadLine();
+    string? Solution = Num1 + Num2;
+    Console.WriteLine(Num2 + ", alright! Thats easy. " + Num1 + " + " + Num2 + " = " + Solution + "!");
+
+}
+else if (opp == "-")
+{
+
+}
+else if (opp == "*")
+{
+
+}
+else if (opp == "/")
+{
+
+}
+else 
+{
+    Console.WriteLine("Invalid Opperator!");
+}
