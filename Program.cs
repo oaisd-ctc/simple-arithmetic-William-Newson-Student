39268866bd713980481bdfa96e68ed8eeaa7b61c
William-Newson-Student <wine2a0@student.oaisd.org>
2024-10-15 00:14:27 +0000

    finnished calculator!

--- Program.cs
+++ Program.cs
@@ -1,29 +1,92 @@
-﻿Console.Clear();
-Console.WriteLine("what operator?");
-string? opp = Console.ReadLine();
-if (opp == "+") 
+﻿static void Calc()
 {
-    Console.WriteLine("Provide 1 number to perform the addition operation");
-    string? Num1 = Console.ReadLine();
-    Console.WriteLine(Num1 + " …got it. Provide a number to add to " + Num1);
-    string? Num2 = Console.ReadLine();
-    string? Solution = Num1 + Num2;
-    Console.WriteLine(Num2 + ", alright! Thats easy. " + Num1 + " + " + Num2 + " = " + Solution + "!");
+    Console.WriteLine("what operator? (+, -, *, /, %)");
+    string? opp = Console.ReadLine();
+    if (opp == "+") 
+    {
+        Console.WriteLine("Provide 1 number to perform the addition operation");
+        float Num1 = float.Parse(Console.ReadLine());
+        Console.WriteLine(Num1 + " …got it. Provide a number to add to " + Num1);
+        float Num2 = float.Parse(Console.ReadLine());
+        float? Solution = Num1 + Num2;
+        Console.WriteLine(Num2 + ", alright! Thats easy. " + Num1 + " + " + Num2 + " = " + Solution + "!");
 
-}
-else if (opp == "-")
-{
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Calc();
+    }
 
-}
-else if (opp == "*")
-{
+    else if (opp == "-")
+    {
+        Console.WriteLine("Provide 1 number to perform the subtraction operation");
+        float Num1 = float.Parse(Console.ReadLine());
+        Console.WriteLine(Num1 + " …got it. Provide a number to subtract to " + Num1);
+        float Num2 = float.Parse(Console.ReadLine());
+        float? Solution = Num1 - Num2;
+        Console.WriteLine(Num2 + ", alright! Thats easy. " + Num1 + " - " + Num2 + " = " + Solution + "!");
+        
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Calc();
+    }
 
-}
-else if (opp == "/")
-{
+    else if (opp == "*")
+    {
+    Console.WriteLine("Provide 1 number to perform the multiplactaion operation");
+        float Num1 = float.Parse(Console.ReadLine());
+        Console.WriteLine(Num1 + " …got it. Provide a number to multiply to " + Num1);
+        float Num2 = float.Parse(Console.ReadLine());
+        float? Solution = Num1 * Num2;
+        Console.WriteLine(Num2 + ", alright! Thats easy. " + Num1 + " * " + Num2 + " = " + Solution + "!");
+        
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Calc();
+    }
 
+    else if (opp == "/")
+    {
+    Console.WriteLine("Provide 1 number to perform the division operation");
+        float Num1 = float.Parse(Console.ReadLine());
+        Console.WriteLine(Num1 + " …got it. Provide a number to divide to " + Num1);
+        float Num2 = float.Parse(Console.ReadLine());
+        float? Solution = Num1 / Num2;
+        Console.WriteLine(Num2 + ", alright! Thats easy. " + Num1 + " / " + Num2 + " = " + Solution + "!");
+        
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Calc();
+    }
+
+    else if (opp == "%")
+    {
+    Console.WriteLine("Provide 1 number to perform the modulus operation");
+        float Num1 = float.Parse(Console.ReadLine());
+        Console.WriteLine(Num1 + " …got it. Provide a number to Modulate to " + Num1);
+        float Num2 = float.Parse(Console.ReadLine());
+        float? Solution = Num1 % Num2;
+        Console.WriteLine(Num2 + ", alright! Thats easy. " + Num1 + " % " + Num2 + " = " + Solution + "!");
+        
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Calc();
+    }
+    else 
+    {
+        Console.WriteLine("Invalid Opperator!");
+        Console.WriteLine("try writing (+, -, *, /, or %)");
+        
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Calc();
+    }
 }
-else 
-{
-    Console.WriteLine("Invalid Opperator!");
-}
+
+Console.Clear();
+Calc();
